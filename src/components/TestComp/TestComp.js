ec209be036777927850b3700741dd50d388590a1
Kevin Peng <kevinpkf311@gmail.com>
2020-10-17 12:32:29 -0700

    Ran create-react-app

---
+++ src/components/TestComp/TestComp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './TestComp.css';
+
+function TestComp(props) {
+    return (
+        <h1 className="header">Hello friends</h1>
+    )
+}
+
+export default TestComp;
