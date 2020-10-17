37f79465c45786b5566e24ef0fdefb977af71462
Kevin Peng <kevinpkf311@gmail.com>
2020-10-17 15:12:51 -0700

    updated plot

--- src/components/Plot/Plot.js
+++ src/components/Plot/Plot.js
@@ -1,11 +1,12 @@
 import React from "react";
 import House from "../House/House.js"
+import house1 from "../../assets/house1.png"
 
 function Plot(props) {
 
 
     return (
-        <House image={"assets/house-1.png"} width={200} height={200}></House>
+        <House image={house1} width={300} height={300}></House>
     )
 }
 
