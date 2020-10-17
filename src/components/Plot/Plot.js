a9cf81680be345fbcb4fae4c6d3dd860ef834057
Kevin Peng <kevinpkf311@gmail.com>
2020-10-17 14:32:57 -0700

    Created basic House and Plot components

---
+++ src/components/Plot/Plot.js
@@ -0,0 +1,12 @@
+import React from "react";
+import House from "../House/House.js"
+
+function Plot(props) {
+
+
+    return (
+        <House image={"assets/house-1.png"} width={200} height={200}></House>
+    )
+}
+
+export default Plot;
