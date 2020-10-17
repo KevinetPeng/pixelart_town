6a201b185a9f3ff0c62f270b98d6287d4e915416
Kevin Peng <kevinpkf311@gmail.com>
2020-10-17 16:28:09 -0700

    Added bg grass texture

--- src/components/Plot/Plot.js
+++ src/components/Plot/Plot.js
@@ -1,6 +1,7 @@
 import React from "react";
 import House from "../House/House.js"
 import house1 from "../../assets/house1.png"
+import grass1 from "../../assets/grass1.png"
 
 function Plot(props) {
 
@@ -8,9 +9,10 @@ function Plot(props) {
     return (
         <div style={{
             display: "inline-block",
-            height: "300px",
-            width: "300px",
+            height: "250px",
+            width: "250px",
             border: "2px solid black",
+            backgroundImage: `url(${grass1})`
         }}>
             <House image={house1}></House>
         </div>
